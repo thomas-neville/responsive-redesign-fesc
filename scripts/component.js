8e68dbd383aac4180fb0d926ebbb5a605babcbe8
Thomas Neville <tnev@uw.edu>
2018-05-29 20:59:56 -0700

    add js files for all pages with vue app data, and add vue scripts and app div to all pages

--- scripts/component.js
+++ scripts/component.js
@@ -1,4 +1,4 @@
 Vue.component('app-footer', {
 	props: ['text'],
-	template: '<h3>Universities and Colleges of the Consortium</h3><div class="universities"></div>'
+	template: '<h3>Universities and Colleges of the Consortium</h3>'
 })
